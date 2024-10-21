6eea8855259463e327b120dc21cf78eb237b052a
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-21 10:39:20 -0400

    init commit

---
+++ Tip Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Tip_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tip Calculator");
+
+            String choice;
+
+            //start loop
+            do
+            {
+                Console.Write("Enter cost of meal: ");
+                decimal mealCost = decimal.Parse(Console.ReadLine());
+
+                decimal tip = mealCost * .15m;
+                decimal total = mealCost + tip;
+
+                //format
+                string formattedTotal = total.ToString("C2");
+                string formattedTip = tip.ToString("C2");
+
+                Console.WriteLine($"Tip amount: {formattedTip}");
+                Console.WriteLine($"TOtal amount: {formattedTotal}");
+
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
